b0b5868ad050fd8fb99b5e2b1f0efc3150f55c1a
jay-bnk48 <tkusonchukul@gmail.com>
2020-06-21 20:45:25 +0700

    master

---
+++ simple.js
@@ -0,0 +1,11 @@
+w='Company';
+x='Address';
+y='Postcode';
+z='Email';
+v=['Hya Co.,Ltd.','Nakornpathom','73210','someone@gmail.com'];
+
+
+for(let i=1;i<=5;i++){
+    document.writeln(w + x + y + z );
+    document.writeln(v[0] + v[1] + v[2] + v[3] );
+}
